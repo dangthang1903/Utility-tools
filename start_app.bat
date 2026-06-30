7f736ce5cfed296edcec9dc37a2ee374832c40a9
dangthang1903 <dangthangkk2000@gmail.com>
2026-06-30 11:39:59 +0700

    feat: add start_app.bat script to launch the frontend development server

---
+++ start_app.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Di chuyen den thu muc frontend cua du an Utility-tools
+cd /d "d:\code\Utility-tools\frontend"
+
+:: Chay lenh start app (thay doi lenh nay neu ban dung lenh khac de chay)
+npm run dev
